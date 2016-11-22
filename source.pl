4fc36a5f157a4d9e072d98c67a8dae5681b2ec1a
crazy-blazer <hajduk1@onet.pl>
2016-11-22 17:15:48 +0100

    source code

--- source.pl
+++ source.pl
@@ -1,11 +1,17 @@
 use WWW::Mechanize::Firefox;
-my $adminPanel  = 'https://example.com/admin/';
+my $adminPanel  = 'https://example.com/admin/'; # Admin Panel URL
+
 my $mech = WWW::Mechanize::Firefox->new( autoclose => 0);
+my $limit = 200; # This value change on own risk!
 
 while(1){
-  $mech->get($adminPanel.'stock/list/page/1?limit=200');
-  while($mech->content !~ /main-table-checkbox-0/){}
+  if($adminPanel !~ /\/$/){
+    $adminPanel .= '/';
+  }
+  my $productList = $adminPanel.'stock/list/page/1?limit='.$limit;
+  $mech->get($productList);
+  while($mech->content !~ /main-table-checkbox-0/){#sleep(1);}
   $mech->click({ xpath => '//input[@id="main-table-checkbox-0"]' , synchronize => 0});
-  while($mech->content !~ /data-confirm-submit="1"/){}
+  while($mech->content !~ /data-confirm-submit="1"/){#sleep(1);}
   $mech->click({ xpath => '//button[@data-confirm-submit="1"]' , synchronize => 0});
 }
